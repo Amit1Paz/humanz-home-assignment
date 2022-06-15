b9668537c004c40b7dfccbe27801902b4f400fbc
unknown <Amit1paz@gmail.com>
2022-06-15 18:10:05 +0300

    (Feat) Atlas db connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+const ATLAS = process.env.ATLAS;
 
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose.connect(ATLAS);
+mongoose.connection
+  .once("open", () => {
+    console.log("Connected to Atlas db");
+  })
+  .on("error", (error) => {
+    console.log(`error: ${error}`);
+  });
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
