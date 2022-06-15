01cdd8f067eba136c60716177873c1d1eac34ec8
unknown <Amit1paz@gmail.com>
2022-06-15 11:03:14 +0300

    (Patch) header position -> top: 0

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const StyledHeader = styled.div`
   position: fixed;
+  top: 0;
   display: flex;
   justify-content: space-between;
   box-shadow: 0 0.3rem 1rem rgba(0, 0, 0, 10%);
