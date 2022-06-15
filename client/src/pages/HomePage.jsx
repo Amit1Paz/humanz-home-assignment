6ec6c5e53bb91f9bcc6a5b58e677801ced58046b
unknown <Amit1paz@gmail.com>
2022-06-15 19:40:13 +0300

    (Feat) useAxios hook

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,8 +2,14 @@ import Header from "../components/Header/Header";
 import { StyledPage } from "./StyledPage";
 import SearchInput from "../components/SearchInput/SearchInput";
 import Client from "../components/Client/Client";
+import axios from 'axios';
 
 const Homepage = () => {
+  const fetch = async () => {
+    const res = await axios.get('http://localhost:8001/clients')
+    console.log(res)
+  }
+  fetch();
   return (
     <StyledPage>
       <Header />
