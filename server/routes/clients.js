dc99a838482ca6a4c5bd22bddb5ef1a3878c5c69
unknown <Amit1paz@gmail.com>
2022-06-16 19:23:02 +0300

    (Feat) clients middleware

--- server/routes/clients.js
+++ server/routes/clients.js
@@ -3,57 +3,15 @@ const { model } = require("mongoose");
 const router = express.Router();
 const Client = require("../models/client.model");
 
-router.get("/", async (req, res) => {
-  try {
-    const limit = 10;
-    const pages = {
-      current: null,
-      next: null,
-      prev: null,
-    };
-    pages.current = parseInt(req.query.page);
-
-    if (!pages.current) {
-      pages.current = 1;
-    }
-
-    const startIndex = (pages.current - 1) * limit;
-    const endIndex = pages.current * limit;
-
-    const search = {};
-    if (req.query.searchName) {
-      search.Name = req.query.searchName;
-    }
-
-    const clients = await Client.find({ ...search })
-      .sort({ _id: 1 })
-      .limit(limit)
-      .skip(startIndex);
-
-    if (endIndex < (await Client.find({ ...search }).countDocuments())) {
-      pages.next = pages.current + 1;
-    }
-
-    if (startIndex > 0) {
-      pages.prev = pages.current - 1;
-    }
-
-    res.send({
-      pages,
-      clients,
-    });
-  } catch (err) {
-    res.json(err);
-  }
+router.get("/", getClients(Client), (req, res) => {
+  res.send(res.pagination);
 });
 
 router.delete("/:id", async (req, res) => {
   try {
     const clientId = parseInt(req.params.id);
 
-    await Client.deleteOne({ ID: clientId }).then((id) =>
-      console.log(`${clientId} was deleted`)
-    );
+    await Client.deleteOne({ ID: clientId });
 
     const limit = 10;
     const pages = {
@@ -68,7 +26,7 @@ router.delete("/:id", async (req, res) => {
 
     const startIndex = (pages.current - 1) * limit;
     const endIndex = pages.current * limit;
-    
+
     const clients = await Client.find()
       .sort({ _id: 1 })
       .limit(limit)
@@ -81,7 +39,7 @@ router.delete("/:id", async (req, res) => {
     if (startIndex > 0) {
       pages.prev = pages.current - 1;
     }
-    console.log(clients);
+
     res.send({
       pages,
       clients,
@@ -143,5 +101,53 @@ router.post("/", async (req, res) => {
     res.json(err);
   }
 });
+
+function getClients(model) {
+  return async (req, res, next) => {
+    try {
+      const limit = 10;
+      const pages = {
+        current: null,
+        next: null,
+        prev: null,
+      };
+      pages.current = parseInt(req.query.page);
+
+      if (!pages.current) {
+        pages.current = 1;
+      }
+
+      const startIndex = (pages.current - 1) * limit;
+      const endIndex = pages.current * limit;
+
+      const search = {};
+      if (req.query.searchName) {
+        search.Name = req.query.searchName;
+      }
+
+      const clients = await model
+        .find({ ...search })
+        .sort({ _id: 1 })
+        .limit(limit)
+        .skip(startIndex);
+
+      if (endIndex < (await model.find({ ...search }).countDocuments())) {
+        pages.next = pages.current + 1;
+      }
+
+      if (startIndex > 0) {
+        pages.prev = pages.current - 1;
+      }
+
+      res.pagination = {
+        pages,
+        clients,
+      };
+    } catch (err) {
+      res.json(err);
+    }
+    next();
+  };
+}
 
 module.exports = router;
